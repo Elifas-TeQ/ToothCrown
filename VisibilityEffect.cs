b0f08fd0fab7e17e4eb9f5099a22a38ba8dd72f9
Alex <sashagsaban@gmail.com>
2022-01-27 13:48:50 +0200

    Update VisibilityEffect.cs

--- VisibilityEffect.cs
+++ VisibilityEffect.cs
@@ -7,3 +7,24 @@ namespace Utils.VisibilityEffectOnScrollDeterminator
         Hide,
     }
 }
+
+/*
+var visibilityEffect = VisibilityEffectOnScrollDeterminator.Determine(eventArgs, Issues, IsSearchEntryVisible, SearchText);
+if (visibilityEffect == VisibilityEffect.Show)
+{
+    IsSearchEntryVisible = true; // or any animation/transition
+}
+else if (visibilityEffect == VisibilityEffect.Hide)
+{
+    IsSearchEntryVisible = false; // or any animation/transition
+}
+
+if (visibilityEffect != VisibilityEffect.None)
+{
+    // A delay is required:
+    // - because a change of visibility causes a change of size what causes a rapid scroll in the opposite direction
+    // (so, the OnScrolled event is fired and it must not be processed);
+    // - for smooth non-blinking behaviour after "show"/"hide" effect is applied.
+    await Task.Delay(1000);
+}
+*/
